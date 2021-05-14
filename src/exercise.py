0dc0cf767f8dd839a1bf1006035870fe5dd32c1e
MrSullivan <>
2021-05-14 20:15:28 +0000

    corrected spelling mistake

--- src/exercise.py
+++ src/exercise.py
@@ -3,7 +3,7 @@ def main():
     print_text()
 
 def print_text():
-    print("In a whole in the ground there lived a method")
+    print("In a hole in the ground there lived a method")
 
 if __name__ == '__main__':
     main()
