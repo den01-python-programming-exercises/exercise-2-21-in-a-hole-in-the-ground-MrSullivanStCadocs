43adcd8f122360288c140d6b90af0d32dbe000d0
MrSullivan <>
2021-05-14 20:13:19 +0000

    wrote a method, i think, it outputs the message but is that because ofthe print command.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    print_text()
+
+def print_text():
+    print("In a whole in the ground there lived a method")
 
 if __name__ == '__main__':
     main()
